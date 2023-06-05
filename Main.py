698dd296828c0d14adf29535ac16ccd8d4c5ec78
gdpouya <gdpouya@gmail.com>
2023-06-05 13:20:10 +0330

    project done but the app does not end so ..

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import threading
 import time
 cities=[]
-time=0
+timer=0
 cities_name=["BARCELONA","MARSEILLE","GENOA","NAPLES","MESSINA"]
 
 class Central_control():
@@ -10,10 +10,9 @@ class Central_control():
     def request_sent(self,ship):
         self.control_semaphore.acquire()
         dis_city=cities[ship.get_dis()]
-        dis_city.print_data()
-        if(dis_city.boat_num > 0):
+        if(dis_city.boat_num() > 0):
+            print("Permission granted for Ship "+ship.get_ship_id()+" to sail, Time: "+str(timer))
             ship.go()
-            print("Permission granted for Ship "+ship.get_ship_id+"to sail, Time: "+str(time))
         self.control_semaphore.release()
     def start(self):
         with open('.\\Water_Waves\\input.txt','r') as f:
@@ -25,12 +24,16 @@ class Central_control():
                 cities.append(city)
             cities.append(City(4,1,0))
             cities[0].add_box(package_number)
-        end_program=True
         for city in cities:
             city.start()
-            end_program=end_program and city.end()
-        if(~end_program):
-            self.start()
+    def check_end(self):
+        end_program=True
+        if(end_program):
+            for city in cities:
+                end_program=end_program and city.end()
+        else:
+            print("end the program")
+                    
 
 cc=Central_control()
 class Ship(threading.Thread):
@@ -42,15 +45,22 @@ class Ship(threading.Thread):
         self.minimum=minimum
         self.city_num=city_num
         self.city=city
-        self.boxes=0
+        self.working=threading.Semaphore(0)
+        self.work=True
     def run(self):
-        if(self.city.boat_fill()):
-            print(str(self.ship_id)+" is home and asking for permission from "+cities_name[self.city_num]+" to "+cities_name[self.city_num+1]+" , Time: "+str(time))
-            cc.request_sent(self)
+        while(True):
+            if(self.city_num < 4 and self.city.boat_fill()):
+                print(str(self.ship_id)+" is home and asking for permission from "+cities_name[self.city_num]+" to "+cities_name[self.city_num+1]+" , Time: "+str(timer))
+                cc.request_sent(self)
+            else:
+                self.work=False
+                print(str(self.ship_id)+" is sleeped")
+                cc.check_end()
+                self.working.acquire()
             
     def go(self):
         self.moving=True
-        print(str(self.ship_id)+" is sailing with "+str(self.minimum)+" packages to "+cities_name[self.city_num+1]+" , Time: "+str(time))
+        print(str(self.ship_id)+" is sailing with "+str(self.minimum)+" packages to "+cities_name[self.city_num+1]+" , Time: "+str(timer))
         cities[self.city_num].boat(-1)
         time.sleep(5)
         cities[self.city_num+1].add_box(self.minimum)
@@ -63,10 +73,17 @@ class Ship(threading.Thread):
         return self.city_num+1
     def get_ship_id(self):
         return self.ship_id
+    def is_working(self):
+        return self.work
+    def s(self):
+        print(str(self.ship_id)+" is waked up")
+        self.work=True
+        self.working.release()
         
 
 class City():
     def __init__( self , city , ship_number , storage) :
+        super().__init__()
         self.city_name=cities_name[city]
         self.ships = []
         self.ship_in_city=ship_number
@@ -79,11 +96,16 @@ class City():
         for i in range (0,ship_number):
             self.ships.append(Ship(self,city,i,storage))
     def start(self):
+        for ship in self.ships:
+            ship.start()
+            
+    def do_it_again(self):
         if(self.storage < self.ship_storage):
-            self.end_task=True
+                self.end_task=True
         else:
             for ship in self.ships:
-                ship.start()
+                if(~ship.is_working()):
+                    ship.s()
     def end(self):
         return self.end_task
     def print_data(self):
@@ -91,16 +113,20 @@ class City():
     def boat_num(self):
         return self.ship_in_city
     def add_box(self,amount):
+        #print(str(self.city_name)+" storage have "+str(self.storage))
         self.end_task=False
         self.change_storeage_sf.acquire()
         self.storage += amount
         self.change_storeage_sf.release()
         for ship in self.ships:
-            if(~ship.is_moving()):
-                ship.start()
+            #print("try to wake "+str(ship.get_ship_id())+" up")
+            #print(str(ship.get_ship_id())+" moving: "+str(ship.is_moving())+" working: "+str(ship.is_working()))
+            if(not ship.is_moving() and not ship.is_working()):
+                ship.s()
+        #print(str(self.city_name)+" storage have "+str(self.storage))
     def boat_fill(self):
         self.change_storeage_sf.acquire()
-        if(self.storage > self.ship_storage):
+        if(self.storage >= self.ship_storage):
             self.storage -= self.ship_storage
             if(self.storage<self.ship_storage):
                 self.end_task=True
@@ -111,10 +137,12 @@ class City():
             return False
     def boat(self,i):
         self.boat_arrive.acquire()
-        self.boat_num += i
+        self.ship_in_city += i
         self.boat_arrive.release()
+    def joins(self):
+        for ship in self.ships:
+            ship.join()
         
 
 if __name__ == '__main__':
     cc.start()
-    print("end the program Bye Bye")
